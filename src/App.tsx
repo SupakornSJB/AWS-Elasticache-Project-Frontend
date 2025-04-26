14aa47dd2cdea1f94a31458076701214d6c9f934
SupakornS <supakorn.sen@outlook.com>
2025-04-26 20:19:34 +0700

    Fix UI (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [isGlobalCache, setIsGlobalCache] = useState(false);
+  const [isCacheFlushed, setIsCacheFlush] = useState(false);
 
   useEffect(() => {
     const localId = localStorage.getItem("clientId");
@@ -40,6 +41,7 @@ function App() {
   const fetchSearch = () => {
     setIsLoading(true);
     setIsError(false);
+    setIsCacheFlush(false);
     const initTime = performance.now();
     let url = import.meta.env.VITE_BACKEND_URL + '/search/' + queryString;
     if (!isGlobalCache) {
@@ -53,6 +55,7 @@ function App() {
         setTime(performance.now() - initTime);
       }).catch(() => {
         setIsError(true);
+        setShownResult(null);
       }).finally(() => {
         setIsLoading(false);
       });
@@ -61,6 +64,7 @@ function App() {
   const flushCache = () => {
     setIsLoading(true);
     setIsError(false);
+    setIsCacheFlush(false);
     let url = import.meta.env.VITE_BACKEND_URL + '/cache/flush';
     if (!isGlobalCache) {
       url += '/' + clientId;
@@ -69,6 +73,7 @@ function App() {
     fetch(url, { method: "DELETE" })
       .finally(() => {
         setIsLoading(false);
+        setIsCacheFlush(true);
       });
   }
 
@@ -78,7 +83,7 @@ function App() {
 
   return (
     <>
-      <div className='flex justify-between items-center gap-2'>
+      <div className='flex justify-between items-center gap-2 flex-wrap'>
         <div className='grow-1'>
           <fieldset className="fieldset border-base-300 rounded-box border p-4">
             <legend className="fieldset-legend">Search</legend>
@@ -99,6 +104,15 @@ function App() {
           <button onClick={flushCache} className='btn btn-dash btn-secondary mt-2'>{isGlobalCache ? "Flush ALL Cache" : "Flush My Cache"}</button>
         </fieldset>
       </div>
+      {
+        isLoading && <h1 className='text-3xl font-bold my-3'>Loading...</h1>
+      }
+      {
+        isError && <h1 className='text-3xl font-bold text-red-500 my-3'>An Error Has Occurred</h1>
+      }
+      {
+        isCacheFlushed && <h1 className='text-3xl font-bold text-success my-3'>Cached Flushed</h1>
+      }
       {
         shownResult &&
         <>
@@ -123,21 +137,17 @@ function App() {
               <div className="stat-desc">Source of the data (Redis/SQL)</div>
             </div>
           </div>
+          <div className="stats shadow">
+            <div className="stat">
+              <div className="stat-title">Queried Record</div>
+              <div className="stat-value">{shownResult.data.length}</div>
+              <div className="stat-desc">Total rows from this query (Only 15 will be shown in the Table)</div>
+            </div>
+          </div>
         </>
-        // <div>
-        //   <span>Time: {time}</span>
-        //   {shownResult.source && <span>Source: {shownResult?.source}</span>}
-        //   {shownResult.hostname && <span>Hostname: {shownResult?.hostname}</span>}
-        // </div>
-      }
-      {
-        isLoading && <h1 className='text-3xl font-bold'>Loading...</h1>
-      }
-      {
-        isError && <h1 className='text-3xl font-bold text-red-500'>An Error Has Occurred</h1>
       }
       <>
-        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 my-3">
           <table className='table'>
             <thead>
               <tr>
@@ -151,7 +161,7 @@ function App() {
             <tbody>
               {
                 (shownResult?.data && shownResult.data.length) ?
-                  (shownResult.data.map((data) => (
+                  (shownResult.data.slice(0, 15).map((data) => (
                     <tr key={data.id}>
                       <td>{data.id}</td>
                       <td>{data.first_name}</td>
@@ -163,10 +173,11 @@ function App() {
               }
             </tbody>
           </table>
-          {
-            !shownResult || !shownResult.data || !shownResult.data.length && <h1 className='text-3xl text-red-500 font-bold'>No Data</h1>
-          }
         </div>
+        {
+          (!shownResult || !shownResult.data || !shownResult.data.length) ? <h1 className='text-3xl  font-bold'>No Data</h1> :
+            <span className='my-3 text-xl font-bold'>Maximum of 15 records will be shown</span>
+        }
       </>
     </>
   )
