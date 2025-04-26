96f5a9be3bec00be46da6e4153014c4898199cf6
SupakornSJB <supakorn.sen@outlook.com>
2025-04-26 15:36:14 +0700

    config dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 import dotenv from "dotenv";
+dotenv.config();
+
+const backendUrl = process.env.VITE_BACKEND_URL;
 
 // https://vite.dev/config/
 export default defineConfig({
