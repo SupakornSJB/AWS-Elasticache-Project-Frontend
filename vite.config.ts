35dad85d8c54f51c799fe930d7161c62b1d1c27a
SupakornSJB <supakorn.sen@outlook.com>
2025-04-26 15:32:37 +0700

    config cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import dotenv from "dotenv";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    cors: import.meta.VITE_BACKEND_URL
+  }
 })
