32241e85715c8dae5b465813daa22a87f93ad1ca
SupakornS <supakorn.sen@outlook.com>
2025-04-26 15:52:42 +0700

    Configure CORS and ENV  (#1)
    * config cors
    
    * install dotenv
    
    * config dotenv
    
    * add type node

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import dotenv from "dotenv";
+dotenv.config();
+
+const backendUrl = process.env.VITE_BACKEND_URL;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    cors: {
+      origin: backendUrl
+    }
+  }
 })
+
